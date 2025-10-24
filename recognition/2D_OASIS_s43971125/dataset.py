572a4ca1b730e452d59ff138e510941371e9067e
s4397112 <s4397112@login0.compute.eait.uq.edu.au>
2025-10-24 10:52:19 +1000

    Updated dataset.py to be functional with the OASIS dataset. Added functions for loading 2D data provided by assignment - Realised after that they aren't necessary for the OASIS dataset, but as I hope to tackle the HipMRI dataset if I can complete this task, I have left them in for now. Will remove before submission if they are still unused.

--- recognition/2D_OASIS_s43971125/dataset.py
+++ recognition/2D_OASIS_s43971125/dataset.py
@@ -1,43 +1,88 @@
 # Contains the data loader
 
+
 import os
 import torch
 import torch.utils.data import Dataset, Dataloader
 import nibabel as nib
 import numpy as np
-
+from PIL import Image
+from tqdm import tqdm
+from glob import glob
 
 class OASISDataset(Dataset):
-    def __init__(self, root_dir="/home/groups/comp3710/OASIS", split="train"):
-        self.root_dir = root_dir
-        self.split=split
-        # filepaths go here
-        self.image_paths=[]
-        self.label_paths=[]
+    def __init__(self, root_dir="/home/groups/comp3710/OASIS", split="train", categorical=false):
+        
+        image_dir = os.path.join(root_dir, f"keras_png_slices_{split}")
+        label_dir = os.path.join(root_dir, f"keras_png_slices_seg_{split}")
+        
+        self.image_paths = sorted(glob.glob(os.path.join(image_dir, "*.png")))
+        self.label_paths = sorted(glob.glob(os.path.join(label_dir, "*.png")))
+
+        self.transform = transform
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
         # load images and labels
-        image = nib.load(self.image_paths[idx]).get_fdata()
-        label = nib.load(self.label_paths[idx]).get_fdata()
+        image = Image.open(self.image_paths[idx]).convert("L")  
+        label = Image.open(self.label_paths[idx]).convert("L")  
 
         # Convert to tensors
-        image = torch.tensor(image, dtype=torch.float32).unsqueeze(0)  # Add channel dim
-        label = torch.tensor(label, dtype=torch.long)
+        image = torch.tensor(np.array(image), dtype=torch.float32).unsqueeze(0) / 255.0
+        label = torch.tensor(np.array(label), dtype=torch.long)
+
+        if self.transform:
+            image = self.transform(image)
 
         return image, label
 
+    #Functions copied from Assignment - will be used if switch from easy to normal task
+    #Unnecessary for OASIS dataset as it is already in PNG format
+    def to_channels(arr: np.ndarray, dtype=np.uint8) -> np.ndarray:
+        channels = np.unique(arr)
+        res = np.zeros(arr.shape + (len(channels),),dtype=dtype)
+        for c in channels:
+            c = int(c)
+            res[..., c:c+1][arr == c] = 1
 
-def get_dataloaders(batch_size=4):
-    # Returns the pytorch dataloaders for our 3 datasets
-    train_dataset = OASISDataset(split="train")
-    val_dataset = OASISDataset(split="val")
-    test_dataset = OASISDataset(split="test")
+        return res
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+    def load_data_2D(imageNames, normImage=False, categorical=False, 
+            dtype=np.float32, getAffines=False, early_stop=False):
+        affines = []
 
-    return train_loader, val_loader, test_loader
+        num = len(imageNames)
+        first_case = nib.load(imageNames[0]).get_fdata(caching='unchanged')
+        if len(first_case.shape) == 3:
+            first_case = first_case[:,:,0] #remove extra dimension
+        if categorical:
+            first_case = to_channels(first_case, dtype=dtype)
+            rows, cols, channels = first_case.shape
+            images = np.zeros((num, rows, cols, channels), dtype=dtype)
+        else:
+            rows, cols = first_case.shape
+            images = np.zeros((num, rows, cols), dtype=dtype)
+
+        for i, inName in enumerate(tqdm(imageNames)):
+            niftiImage = nib.load(inName)
+            inImage = niftiImage.get_fdata(caching='unchanged')
+            affine = niftiImage.affine
+            if len(inImage.shape) == 3:
+            inImage = inImage[:,:,0]
+            inImage = inImage.astype(dtype)
+            if normImage:
+                inImage = (inImage - inImage.mean()) / inImage.std()
+            if categorical:
+                inImage = utils.to_channels(inImage, dtype=dtype)
+                images[i,:,:,:] = inImage
+            else:
+                images[i,:,:] = inIMage
+            affines.append(affine)
+            if i > 20 and early_stop:
+                break
+        if getAffines:
+            return images, affines
+        else:
+            return images
